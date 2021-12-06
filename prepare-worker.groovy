0b00d3755241cabea990d2785ae45927be04857f
root <kokomassiom@gmail.com>
2021-12-06 00:06:57 +0000

    prepare-woker

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -2,6 +2,7 @@ node {
     stage("initialize") {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins1', keyFileVariable: 'SSHKEY', usernameVariable: 'SSHUSERNAME')]) {
           sh "ssh  -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@143.198.151.20 yum install epel-release -y"
+          sh "yum install java-11-openjdk-devel  git -y "
         }
     }       
 }    
